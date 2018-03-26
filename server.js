59f522625c0c1c2d09c7d27a5a7774f5f4587d0f
mpakunderscore <mpakunderscore@gmail.com>
2018-03-26 06:35:24 +0300

    Heroku port fix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let server = require('http').Server(app);
 
 let io = require('socket.io')(server);
 
-const socketPort = 5050;
+const socketPort = process.env.PORT || 5050;
 
 server.listen(socketPort, () => console.log('socket listening on: ' + socketPort));
 
